fa84413ff498d484a147582c80aa423125cc61eb
Ryan <ryan.kromdijk@gmail.com>
2025-03-24 15:55:34 +0100

    Setup (zonder module)

---
+++ src/main/java/module-info.java
@@ -0,0 +1,3 @@
+ module {
+    requires hanyaeger;
+         }
---
+++ src/main/java/org/example/AllTheWayUp.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import com.github.hanyaeger.api.YaegerGame;
+
+/**
+ * Hello world!
+ *
+ */
+public class AllTheWayUp extends YaegerGame
+{
+    public static void main( String[] args ) {
+        launch(args);
+    }
+
+    @Override
+    public void setupGame() {
+
+    }
+
+    @Override
+    public void setupScenes() {
+//        addScene(0, new MenuScene(this));
+//        addScene(1, new GameScene(this));
+//        addScene(2, new GameOverScene(this));
+    }
+}
