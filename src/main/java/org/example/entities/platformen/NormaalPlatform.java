f409ea3dc9f7712bcd27d8f7194de0f610406c40
Ryan <ryan.kromdijk@gmail.com>
2025-03-25 15:05:33 +0100

    NormaalPlatform beginnetje

--- src/main/java/org/example/entities/platformen/NormaalPlatform.java
+++ src/main/java/org/example/entities/platformen/NormaalPlatform.java
@@ -1,49 +1,14 @@
 package org.example.entities.platformen;
 
-import com.github.hanyaeger.api.EntitySpawnerContainer;
+import com.github.hanyaeger.api.Coordinate2D;
+import com.github.hanyaeger.api.Size;
 import com.github.hanyaeger.api.entities.Collider;
-import com.github.hanyaeger.api.entities.EntitySpawner;
-import com.github.hanyaeger.core.entities.EntityCollection;
-import com.google.inject.Injector;
-import javafx.scene.Node;
-import javafx.scene.layout.Pane;
 
-import java.util.List;
-import java.util.Optional;
+import com.github.hanyaeger.api.entities.impl.SpriteEntity;
 
-public class NormaalPlatform implements IPlatformSpawner, Collider, EntitySpawnerContainer {
-    @Override
-    public void onCollision() {
 
-    }
-
-    @Override
-    public Optional<? extends Node> getNode() {
-        return Optional.empty();
-    }
-
-    @Override
-    public void setupEntitySpawners() {
-
-    }
-
-    @Override
-    public Injector getInjector() {
-        return null;
-    }
-
-    @Override
-    public Pane getRootPane() {
-        return null;
-    }
-
-    @Override
-    public EntityCollection getEntityCollection() {
-        return null;
-    }
-
-    @Override
-    public List<EntitySpawner> getSpawners() {
-        return List.of();
+public class NormaalPlatform extends SpriteEntity implements Collider {
+    public NormaalPlatform(Coordinate2D initialLocation, Size grootte, String afbeelding) {
+        super(afbeelding, initialLocation, grootte);
     }
 }
